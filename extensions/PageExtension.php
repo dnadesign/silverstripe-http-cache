3d21a559fd3afaf3fb371df85f959fe63ff23bed
rafaeldsousa <39249777+rafaeldsousa@users.noreply.github.com>
2019-08-08 11:52:51 +1200

    Update PageExtension.php
    Replacing Numeric field with TextField. Numeric field isn't saving value in db

--- extensions/PageExtension.php
+++ extensions/PageExtension.php
@@ -7,7 +7,7 @@ use SilverStripe\Security\Member;
 use SilverStripe\Security\Permission;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\Forms\FieldList;
-use SilverStripe\Forms\NumericField;
+use SilverStripe\Forms\TextField;
 
 /**
  * This extension adds the ability to control the max-age per originator.
@@ -39,13 +39,13 @@ class PageExtension extends DataExtension
 
         /* http_cache_disable can be used on subclasses to override the behaviour */
         if ($this->owner->Config()->get('http_cache_disable')) {
-            $maxAge = NumericField::create('MaxAgeOverriden', 'Custom cache timeout [minutes]', 0);
+            $maxAge = TextField::create('MaxAgeOverriden', 'Custom cache timeout [minutes]', 0);
             $maxAge->setDisabled(true);
 
             $maxAge->setDescription('This field controls the length of time the page will be cached for.<br/>' .
                 'Overriden in the site configuration');
         } else {
-            $maxAge = NumericField::create('MaxAge', 'Custom cache timeout [minutes]');
+            $maxAge = TextField::create('MaxAge', 'Custom cache timeout [minutes]');
             $maxAge->setDescription('This field controls the length of time the page will be cached for.<br/>' .
                 'You will not be able to see updates to this page for at most the specified amount of minutes.<br/>' .
                 'Leave empty to set back to the default which is <strong>' . ($default / 60) . ' mins</strong>. ' .
